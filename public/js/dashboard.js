77a6ae1ecba7ead67b95ab288a6bf3b1f44543aa
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-02-15 17:35:35 -0500

    added skeleton functionality for modal for adding new entries

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,4 @@
+// Side Nav
 var instance = M.Sidenav.getInstance(elem);
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -13,4 +14,18 @@ document.getElementsByClassName('Sidenav-trigger').addEventListener('click', fun
 // Listener to close Side Nav
 document.getElementsByClassName('Sidenav-close').addEventListener('click', function () {
     instance.close();
+});
+
+
+// Modal New Entry
+var instance = M.Modal.getInstance(elem);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+// Listener for Add Entry button
+document.getElementsById('addNew').addEventListener('click', function () {
+    instance.open();
 });
