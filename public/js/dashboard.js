4e191c364b9ae8f2d8c91c187ce49f3c7736de6c
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-02-15 19:15:16 -0500

    added modal js functionality for pop up window

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,17 +2,8 @@
 const slideMenu = document.querySelectorAll(".sidenav");
 M.Sidenav.init(slideMenu, {});
 
-
-
 // Modal New Entry
-var instance = M.Modal.getInstance(elem);
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
-
-// Listener for Add Entry button
-document.getElementsById('addNew').addEventListener('click', function () {
-    instance.open();
+document.addEventListener("DOMContentLoaded", function() {
+    const newEntryWindow = document.querySelector(".modal");
+    M.Modal.init(newEntryWindow, {});
 });
