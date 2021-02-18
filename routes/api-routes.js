251ad23e43837bb189a51c619f52f4f90ab564a3
Delph <delph_teb@hotmail.com>
2021-02-18 00:54:55 -0500

    complete basic api routes and list

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,9 +4,23 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 module.exports = (app) => {
+
+  // function to delete the display of the password and ThemeId
+  const delPass = (val) => {
+    delete val.dataValues.password;
+    delete val.dataValues.ThemeId;
+    return val;
+  };
+
+  // function to delete the display of the password and ThemeId when associated table
+  const delPass2 = (val) => {
+    delete val.dataValues.User.dataValues.password;
+    delete val.dataValues.User.dataValues.ThemeId;
+    return val;
+  };
+
   // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
+  // If the user has valid login credentials, send them to the members page. Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local"), (req, res) => {
     res.json(req.user);
   });
@@ -20,10 +34,10 @@ module.exports = (app) => {
       password: req.body.password
     })
       .then(() => {
-        res.redirect(307, "/api/login");
+        res.redirect(307, "/api/login"); // Temporary Redirect status
       })
       .catch(function (err) {
-        res.status(401).json(err);
+        res.status(401).json(err); // Unauthorized status
       });
   });
 
@@ -33,17 +47,32 @@ module.exports = (app) => {
     res.redirect("/");
   });
 
-  // GET route for getting all of the quotes
-  app.get("/api/quotes", (req, res) => {
-    db.Quote.findAll({})
-      .then((result) => {
-        res.json(result);
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", (req, res) => {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email,
+        id: req.user.id
       });
+    }
   });
 
   // GET route for getting all of the themes
   app.get("/api/themes", (req, res) => {
     db.Theme.findAll({})
+      .then((result) => {
+        res.json(result);
+      });
+  });
+
+  // GET route for getting all of the quotes
+  app.get("/api/quotes", (req, res) => {
+    db.Quote.findAll({})
       .then((result) => {
         res.json(result);
       });
@@ -65,20 +94,7 @@ module.exports = (app) => {
       });
   });
 
-  // GET route for getting all of the users with theme
-  app.get("/api/users", (req, res) => {
-    db.User.findAll({include:[db.Theme]})
-      .then((result) => {
-        //Excluding password from the result
-        const userInfo = [{
-          id: result.id,
-          email: result.email
-        }];
-        res.json(userInfo);
-      });
-  });
-
-  // GET route for retrieveing a single user
+  // GET route for retrieveing a single user with theme
   app.get("/api/users/:id", (req, res) => {
     db.User.findOne({
       where: {
@@ -86,40 +102,66 @@ module.exports = (app) => {
       },
       include:[db.Theme]
     }).then((result) => {
-      res.json(result); // **********TO DO: Excluding password from the array result
-    });
-  });
-
-  // GET route for retrieveing all user data
-  app.get("/api/userdata/:id", (req, res) => {
-    db.UserData.findOne({
-      where: {
-        id: req.params.id
-      },
-      include:[db.User, db.Mood, db.Activity]
-    }).then((result) => {
+      delPass(result); // Excluding password and unnessary key from result
       res.json(result);
     });
   });
 
-
-  // *************TO DO: ADD THEME ************** //
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", (req, res) => {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
+  // GET route for retrieveing all data from current user
+  app.get("/api/userdata/:userId", (req, res) => {
+    db.UserData.findAll({
+      where: {
+        userId: req.params.userId
+      },
+      include:[db.User, db.Mood, db.Activity]
+    }).then((result) => {
+      result.forEach((val) => {
+        delPass2(val); // Excluding password and unnessary key from result
       });
-    }
+      res.json(result);
+    });
   });
 
+  // PUT route for updating the theme of the current user
+  app.put("/api/users/:id", (req, res) => {
+    db.User.update(
+      {ThemeId: req.body.ThemeId},
+      {where: req.params.id}
+    ).then((result) => {
+      delPass(result); // Excluding password and unnessary key from result: ?NECESSARY?
+      res.status(202).send(result); // Accepted status
+    }).catch((error) => {
+      console.log(error); // FOR TESTING
+      res.status(400).send(error); // Bad request status
+    });
+  });
 
-  // *************TO DO ************** //
-// Get route for returning UserData including Mood, Activity and User
+  // POST route for adding mood and activity to the current user
+  app.post("/api/userdata", (req, res) => {
+    db.UserData.create({
+      userId: req.body.userId,
+      activityId: req.body.activityId,
+      moodId: req.body.moodId
+    }).then((result) => {
+      console.log(result); // FOR TESTING
+      res.status(201).send(result); // Created status
+    }).catch((error) => {
+      console.log(error);
+      res.status(400).send(error); // Bad request status
+    });
+  });
+
+  /***************MAY NOT BE NEEDED*************/
+
+  // GET route for getting all of the users with theme
+  app.get("/api/users", (req, res) => {
+    db.User.findAll({include:[db.Theme]})
+      .then((result) => {
+        result.forEach((val) => {
+          delPass(val); // Excluding password and unnessary key from result
+        });
+        res.json(result);
+      });
+  });
+
 };
