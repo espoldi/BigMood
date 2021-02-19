0269aec83f2002d8a4dab8f6c7aecd50d8d92f42
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-02-18 19:56:23 -0500

    fixed sort input field

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -15,8 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Dropdown for sorting all entries
-const sortBy = document.querySelector(".dropdown-trigger");
-M.Dropdown.init(sortBy, {});
+document.addEventListener("DOMContentLoaded", function() {
+const sortBy = document.querySelector("select");
+M.FormSelect.init(sortBy, {});
+});
 
 // Collapsible for all entries
 document.addEventListener("DOMContentLoaded", function() {
