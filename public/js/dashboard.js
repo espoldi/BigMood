7dcb728d232f79cf79a63dc5faa39f84320b1aea
Delph <delph_teb@hotmail.com>
2021-02-20 00:55:17 -0500

    add quote call

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,24 +4,33 @@ M.Sidenav.init(slideMenu, {});
 
 // Statistics tabs
 document.addEventListener("DOMContentLoaded", function() {
-    const stats = document.querySelector(".tabs");
-    M.Tabs.init(stats, {});
+  const stats = document.querySelector(".tabs");
+  M.Tabs.init(stats, {});
+  // GetRandom quote at loading page
+  $.get("/api/quotes").then(function(data) {
+    let quote;
+    quote = data[Math.floor(Math.random() * data.length)];
+    $("#new-quote").text(quote.body);
+    $("#author").text(`— ${quote.author}`);
+  }).catch((err) => {
+    console.log(JSON.stringify(err));
+  });
 });
 
 // Modal New Entry
 document.addEventListener("DOMContentLoaded", function() {
-    const newEntryWindow = document.querySelector(".modal");
-    M.Modal.init(newEntryWindow, {});
+  const newEntryWindow = document.querySelector(".modal");
+  M.Modal.init(newEntryWindow, {});
 });
 
 // Dropdown for sorting all entries
 document.addEventListener("DOMContentLoaded", function() {
-const sortBy = document.querySelector("select");
-M.FormSelect.init(sortBy, {});
+  const sortBy = document.querySelector("select");
+  M.FormSelect.init(sortBy, {});
 });
 
 // Collapsible for all entries
 document.addEventListener("DOMContentLoaded", function() {
-    const displayNew = document.querySelector(".collapsible");
-    M.Collapsible.init(displayNew, {});
+  const displayNew = document.querySelector(".collapsible");
+  M.Collapsible.init(displayNew, {});
 });
