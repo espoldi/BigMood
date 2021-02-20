0dc9969221cb6c3a2d1e62bd489d2a5d2a63c9b3
Delph <delph_teb@hotmail.com>
2021-02-20 18:21:38 -0500

    listener on dropdown and update db

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,28 +1,74 @@
 $(document).ready(function () {
-  let userName;
-  let userId;
+  let userName, themeName;
+  let userId, themeId;
   let userData = [];
+
+  M.AutoInit(); // Initiate dropdown
 
   // Get the current user name and id
   $.get("/api/user_data").then(function (data) {
     userName = data.name; // current username
     userId= data.id; // current user id
     $(".current-user").text(userName);
+
+    console.log(userId);
+    /* Get the current theme for the current user
+ $.get(`/api/users/${userId}`).then(function (data) {
+  ThemeId = data.Theme.id; // current username
+  console.log(userId);
+  console.log(ThemeId);
+}); */
   });
 
-  // Get all previous data from current user
+  // Function to change color class based on the themeId
+  function displayTheme(theme) {
+
+
+    // switch () {
+    //   case :
+
+    //     break;
+    //   case :
+
+    //     break;
+
+    // }
+  }
+
+
+
+  /* Get all previous data from current user
   $.get(`/api/userdata/${userId}`).then(function (data) {
     userData = data;
     console.log(userData); //FOR TESTING
   }).catch((err) => {
     console.log(JSON.stringify(err));
-  });
+  }); */
 
-  // Side Nav Menu
+  /**** Side Nav Menu ****/
   const slideMenu = document.querySelectorAll(".sidenav");
   M.Sidenav.init(slideMenu, {});
 
-  // Statistics tabs
+  // Dropdown listeners
+  $("#dropdown1").click(e => {
+    newTheme = e.target.firstChild.textContent;
+    themeId = e.target.getAttributeNode("data-id").value;
+
+    let updatingUser = {
+      id: userId,
+      ThemeId: themeId
+    };
+    console.log(updatingUser); // FOR TESTING
+    // Update ThemeId for current user in users table
+    $.post("/api/update", updatingUser).then((data) => {
+      location.reload("dashboard");
+    }).catch((err) => {
+      console.log(JSON.stringify(err));
+    });
+  });
+
+
+  /**** Statistics tabs ****/
   const stats = $(".tabs");
   M.Tabs.init(stats, {});
   // Get Random quote at loading page
@@ -35,7 +81,7 @@ $(document).ready(function () {
     console.log(JSON.stringify(err));
   });
 
-  // Modal New Entry
+  /**** Modal New Entry ****/
   const newEntryWindow = document.querySelector(".modal");
   M.Modal.init(newEntryWindow, {});
 
@@ -48,30 +94,6 @@ $(document).ready(function () {
   // Collapsible for all entries
   const displayNew = document.querySelector(".collapsible");
   M.Collapsible.init(displayNew, {});
-
-
-  // Select menu for changing the theme
-  var elems = document.querySelectorAll("select");
-  // var instances = M.FormSelect.init(elems, options);
-
-
-  // switch () {
-  //   case :
-
-  //     break;
-  //   case :
-
-  //     break;
-
-  // }
-
-
-  // const theme = $(".theme");
-  // M.Theme.init(theme, {});
-  // $.post("/api/update").then(function (data) {
-  //   if (theme.classList.contains
-  // });
-
 
 
 
