82e7ebc125b0ba95badf775671574449d5c6a921
Delph <delph_teb@hotmail.com>
2021-02-21 02:56:28 -0500

    fix sidenav

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -42,11 +42,6 @@ module.exports = (app) => {
       });
   });
 
-  // Route for logging user out
-  app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
 
   // Route for getting some data about our user to be used client side
   app.get("/api/user_data", (req, res) => {
