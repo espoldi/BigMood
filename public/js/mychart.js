0660ccf733863bd39ba1a2a89514f94dc9114f30
Jeremy Rice <jdavidrice@gmail.com>
2021-02-22 21:43:49 -0500

    Comment out unused api call in mychart.js and remove empty line in dashboard.handlebars

--- public/js/mychart.js
+++ public/js/mychart.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
 let color = sessionStorage.getItem("color");
 let id = sessionStorage.getItem("userId");
 
 /** Get data from API: TO DO ***/
-$.get(`/api/userdata/${id}`).then(function(data) {
+// $.get(`/api/userdata/${id}`).then(function(data) {
 
-});
+// });
 
 
 // Temporary hardcoded data for Yaxis
